d578887781f82e8a084fb000c6b022d67c7e6604
Roman F <70765447+romdotdog@users.noreply.github.com>
2021-12-06 15:23:49 +0000

    handle realloc failure

--- display.c
+++ display.c
@@ -6,8 +6,7 @@
 #ifndef ALIGNMENT
 #define ALIGNMENT ((int)sizeof(size_t))
 #endif
-#define ALIGN(s) (((s) + (ALIGNMENT)-1) & \
-				  ~((ALIGNMENT)-1))
+#define ALIGN(s) (((s) + (ALIGNMENT)-1) &  ~((ALIGNMENT)-1))
 
 static unsigned int llog10(int64_t x)
 {
@@ -17,10 +16,15 @@ static unsigned int llog10(int64_t x)
 #undef log10c
 }
 
-static void expand(printer_t *ctx)
+static int expand(printer_t *ctx)
 {
 	ctx->cap = ALIGN(ctx->cap << 1);
 	ctx->buf = realloc(ctx->buf, ctx->cap);
+	if (ctx->buf == NULL) {
+		printf("failed to reallocate memory for printing");
+		return -1;
+	}
+	return 0;
 }
 
 static int traverse(printer_t *ctx, symbol_t *res)
@@ -29,21 +33,25 @@ static int traverse(printer_t *ctx, symbol_t *res)
 	{
 	case AT_DIVIDE:
 	{
-		traverse(ctx, res->operation.lhs);
+		if (traverse(ctx, res->operation.lhs) != 0)
+			return -1;
 		int max_size = 4;
 		if (ctx->cap < ctx->offset + max_size)
-			expand(ctx);
+			if (expand(ctx) != 0)
+				return -1;
 		ctx->offset += snprintf(ctx->buf + ctx->offset, max_size, " / ");
-		traverse(ctx, res->operation.rhs);
-		return 0;
+		if (traverse(ctx, res->operation.rhs) != 0)
+			return -1;
+		break;
 	}
 	case AT_INTEGER:
 	{
 		int max_size = llog10(res->integer) + (res->integer < 0) + 2;
 		if (ctx->cap < ctx->offset + max_size)
-			expand(ctx);
+			if (expand(ctx) != 0)
+				return -1;
 		ctx->offset += snprintf(ctx->buf + ctx->offset, max_size, "%" PRId64, res->integer);
-		return 0;
+		break;
 	}
 	}
 	return 0;
